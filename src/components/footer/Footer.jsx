860e13072203b7c6f19d68889852c3074a30b35e
justina177 <justinaenenche@yahoo.com>
2023-05-04 12:38:52 +0100

    Added the Footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Fotter.scss';
+
+const Footer = () => {
+  return (
+    <>
+    <hr className="--color-dark" />
+    <div className="--flex-center --py--bg-grey">
+        <p>All Rights Reserved. &copy; 2023</p>
+    </div>
+
+    </>
+  )
+}
+
+export default Footer
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
 
 const Home = () => {
   return (
     <div>
         <Header />
-        this is the home page
+        <Footer />
+       
     </div>
   )
 }
