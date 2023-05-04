d049f51cfeb7e9a646b26d051e8589f57d9cf0fe
justina177 <justinaenenche@yahoo.com>
2023-05-04 13:06:03 +0100

    Added the Body Section of the home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../../components/footer/Footer'
 import Header from '../../components/header/Header'
+import Section from '../section/Section'
 
 const Home = () => {
   return (
     <div>
         <Header />
+        <Section />
         <Footer />
        
     </div>
---
+++ src/pages/section/Section.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Section.scss';
+import loginImg from '../../assets/login.svg';
+
+const Section = () => {
+  return (
+    <div>
+        <section className="container hero">
+            <div className="hero-text">
+                <h2>MERN Stack Authentication System</h2>
+                <p>
+                    Learn and Master Authentication and 
+                    Authorization Using MERN Stack
+                </p>
+                <p>
+                    Implement User Registration, Login, 
+                    Password Reset, Social Login, User Permissions, 
+                    Email Notifications etc
+                </p>
+                <div className="hero-buttons --flex-start">
+                    <button className="--btn --btn-danger">Register</button>
+                    <button className="--btn --btn-primary">Login</button>
+                </div>
+            </div>
+
+            <div className="hero-image">
+                <img src={loginImg} alt="Auth" />
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Section
